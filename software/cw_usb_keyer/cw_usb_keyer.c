d9dd10074acbb44edf4eafb399c60f73dbb0ccad
Brachyurus <82178904+Brachyurus@users.noreply.github.com>
2026-03-17 01:11:29 +0200

    Update cw_usb_keyer.c

--- software/cw_usb_keyer/cw_usb_keyer.c
+++ software/cw_usb_keyer/cw_usb_keyer.c
@@ -1,48 +1,3 @@
-// ===================================================================================
-// Project:   Rubber Ducky for CH551, CH552 and CH554
-// Version:   v1.1
-// Year:      2023
-// Author:    Stefan Wagner
-// Github:    https://github.com/wagiminator
-// EasyEDA:   https://easyeda.com/wagiminator
-// License:   http://creativecommons.org/licenses/by-sa/3.0/
-// ===================================================================================
-//
-// Description:
-// ------------
-// Identifies itself as a USB HID keyboard and types a message when the touchkey is
-// pressed. It can be used to control the PC via keyboard shortcuts.
-//
-// References:
-// -----------
-// - Blinkinlabs: https://github.com/Blinkinlabs/ch554_sdcc
-// - Deqing Sun: https://github.com/DeqingSun/ch55xduino
-// - Ralph Doncaster: https://github.com/nerdralph/ch554_sdcc
-// - WCH Nanjing Qinheng Microelectronics: http://wch.cn
-//
-// Compilation Instructions:
-// -------------------------
-// - Chip:  CH551, CH552 or CH554
-// - Clock: 16 MHz internal
-// - Adjust the firmware parameters in src/config.h if necessary.
-// - Make sure SDCC toolchain and Python3 with PyUSB is installed.
-// - Press BOOT button on the board and keep it pressed while connecting it via USB
-//   with your PC.
-// - Run 'make flash' immediatly afterwards.
-// - To compile the firmware using the Arduino IDE, follow the instructions in the 
-//   .ino file.
-//
-// Operating Instructions:
-// -----------------------
-// - Connect the board via USB to your PC. It should be detected as a HID keyboard.
-// - Open a text editor und press the touchkey on the board.
-
-
-// ===================================================================================
-// Libraries, Definitions and Macros
-// ===================================================================================
-
-// Libraries
 #include "src/system.h"                   // system functions
 #include "src/usb_keyboard.h"             // USB HID keyboard functions
 #include <stdbool.h>                      // boolean functions
@@ -53,48 +8,62 @@ void USB_ISR(void) __interrupt(INT_NO_USB) {
   USB_interrupt();
 }
 
+
 void delay(void) {
-  for(volatile unsigned int i=0; i<3000; i++);
+  for(volatile unsigned int i=0; i<2000; i++);
 }
 
-// ===================================================================================
-// Main Function
-// ===================================================================================
+
 void main(void) {                         // Setup
   CLK_config();                           // configure system clock
   KBD_init();                             // init USB HID keyboard
 
-  // Configure P1.5 as input with pull-up
-  P1_MOD_OC |= (1 << 5);     // open-drain mode (optional, depends on board)
+  // Configure P1.5 and P1.6 as inputs with internal pull-ups
   P1_DIR_PU &= ~(1 << 5);    // clear output bit → input
   P1_DIR_PU |= (1 << 5);     // enable internal pull-up (1 = pull-up enabled)
 
-  P1_MOD_OC |= (1 << 6);     // open-drain mode (optional, depends on board)
   P1_DIR_PU &= ~(1 << 6);    // clear output bit → input
   P1_DIR_PU |= (1 << 6);     // enable internal pull-up (1 = pull-up enabled)
 
-  bool key_a = true;
-  bool key_b = true;
+  // set initial values for key state buffers
+  bool prev_state_a = true;
+  bool prev_state_b = true;
+
+  // default characters
+  char left_key_char = 'a';
+  char right_key_char = 'b';
+
+  // read both input pins once
+  bool state_a = (P1 & (1 << 5)) != 0;
+  bool state_b = (P1 & (1 << 6)) != 0;
+
+  // if both paddles are pressed while USB device is being plugged in, '[' and ']' characters will be used
+  if (!state_a && !state_b) {
+    left_key_char = '[';
+    right_key_char = ']';
+  }
 
   while (1) {
+    // read both input states
+    bool state_a = (P1 & (1 << 5)) != 0;
+    bool state_b = (P1 & (1 << 6)) != 0;
 
-    bool new_a = (P1 & (1 << 5)) != 0;
-    bool new_b = (P1 & (1 << 6)) != 0;
-
-    if (new_a != key_a) {
-      key_a = new_a;
-      if (key_a) {
-        KBD_release('a');
+    // if current state does not match state buffer, update state buffer and send press\release command based on current state
+    if (state_a != prev_state_a) {
+      prev_state_a = state_a;
+      if (state_a) {
+        KBD_release(left_key_char);
       } else {
-        KBD_press('a');
+        KBD_press(left_key_char);
       }
     }
-    if (new_b != key_b) {
-      key_b = new_b;
-      if (key_b) {
-        KBD_release('b');
+
+    if (state_b != prev_state_b) {
+      prev_state_b = state_b;
+      if (state_b) {
+        KBD_release(right_key_char);
       } else {
-        KBD_press('b');
+        KBD_press(right_key_char);
       }
     }
     delay();
